fbd33daef5cd6956a619486d97040def222836de
Tiberiu Paduraru <PaduraruTiberiu97@gmail.com>
2026-02-12 20:38:01 +0200

    refactor(auth-service): introduce repository abstraction and add authenticate handler tests

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -24,7 +24,7 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// validate the user
-	user, err := app.Models.Users.GetByEmail(requestPayload.Email)
+	user, err := app.Repo.GetByEmail(requestPayload.Email)
 	if err != nil {
 		err := app.errorJSON(w, errors.New("invalid credentials"), http.StatusUnauthorized)
 		if err != nil {
@@ -34,7 +34,7 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// validate password
-	valid, err := user.PasswordMatches(requestPayload.Password)
+	valid, err := app.Repo.PasswordMatches(requestPayload.Password, *user)
 	if err != nil || !valid {
 		err := app.errorJSON(w, errors.New("invalid credentials"), http.StatusUnauthorized)
 		if err != nil {
@@ -79,8 +79,7 @@ func (app *Config) logRequest(name, data string) error {
 		return err
 	}
 
-	client := &http.Client{}
-	_, err = client.Do(request)
+	_, err = app.Client.Do(request)
 	if err != nil {
 		return err
 	}
